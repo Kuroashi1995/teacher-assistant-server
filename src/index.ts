602d55101897f936a7d7caf6a9e28794534322d0
kuroashi1995 <kuroashi.prog@gmail.com>
2023-11-28 12:22:02 -0300

    Installed dependencies and home response

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
+
+const app = express();
+app.use(cors());
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080");
+});
