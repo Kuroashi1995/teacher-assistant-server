61f08da2d8f9fdec32728ccfea077111104d34bc
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-02-06 13:26:24 -0300

    user router initialized

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import compression from "compression";
 import { config } from "./core/config";
+import userRouter from "./user/presentation/user_router";
+import { UserRepository } from "./user/aplication/user_repository_implementation";
+import { UserDatabase } from "./user/infrastructure/user_database";
+import { Database } from "./core/services/db";
 
 const app = express();
 app.use(cors());
@@ -12,10 +16,21 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+//todo: improve routing and naming, route api is missing
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(
+  "/users",
+  userRouter({
+    userRepository: new UserRepository({
+      userDatabase: new UserDatabase({
+        databaseService: new Database(),
+      }),
+    }),
+  })
+);
 const server = http.createServer(app);
 
 server.listen(config.port, () => {
