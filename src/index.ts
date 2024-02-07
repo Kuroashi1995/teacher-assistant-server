58eecb540ee08f6de14775d483e66f880a0a419e
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-02-07 01:17:42 -0300

    add auth router to main file

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ import userRouter from "./user/presentation/user_router";
 import { UserRepository } from "./user/aplication/user_repository_implementation";
 import { UserDatabase } from "./user/infrastructure/user_database";
 import { Database } from "./core/services/db";
+import { authRouter } from "./auth/presentation/auth_router";
+import { AuthRepository } from "./auth/aplication/auth_repository_Implementation";
+import { CredentialsDatabase } from "./auth/infrastructure/credentials_database";
+import { SignUpUseCase } from "./auth/aplication/sign_up_use_case";
 
 const app = express();
 app.use(cors());
@@ -20,6 +24,27 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(
+  "/auth",
+  authRouter({
+    authRepository: new AuthRepository({
+      credentialsDatabase: new CredentialsDatabase({
+        databaseService: new Database(),
+      }),
+    }),
+    signUpUseCase: new SignUpUseCase({
+      authRepository: new AuthRepository({
+        credentialsDatabase: new CredentialsDatabase({
+          databaseService: new Database(),
+        }),
+      }),
+      userRepository: new UserRepository({
+        userDatabase: new UserDatabase({ databaseService: new Database() }),
+      }),
+    }),
+  })
+);
 
 app.use(
   "/users",
