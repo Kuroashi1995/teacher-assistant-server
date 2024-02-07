5e6d3d4f32ab187718eb457f9d6b16b4f137319c
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-02-07 01:16:45 -0300

    create credentials model

---
+++ src/auth/domain/credentials.ts
@@ -0,0 +1,45 @@
+export class Credentials {
+  username?: string;
+  userEmail: string;
+  password: string;
+  userId: number;
+
+  constructor({
+    username,
+    userEmail,
+    password,
+    userId,
+  }: {
+    username?: string;
+    userEmail: string;
+    password: string;
+    userId: number;
+  }) {
+    this.username = username;
+    this.userEmail = userEmail;
+    this.password = password;
+    this.userId = userId;
+  }
+
+  static fromData(data: any) {
+    return new Credentials({
+      password: data.password,
+      userEmail: data.user_email,
+      userId: data.user_id,
+      username: data.username,
+    });
+  }
+
+  toData() {
+    return {
+      password: this.password,
+      user_email: this.userEmail,
+      username: this.username,
+      user_id: this.userId,
+    };
+  }
+
+  update(updatedData: Partial<Credentials>): Credentials {
+    return new Credentials({ ...this, ...updatedData });
+  }
+}
