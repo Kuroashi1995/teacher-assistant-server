9a06ebf64727f7f6e9da262791b034fa7523579f
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-02-09 01:58:14 -0300

    timing attack safe basic auth middleware

--- src/core/middlewares/auth_validator.ts
+++ src/core/middlewares/auth_validator.ts
@@ -23,6 +23,29 @@ export async function authValidator(
       const decoded = Buffer.from(code, "base64").toString();
       const [_, decodedUsername, decodedPassword] =
         decoded.match(/(.*?):(.*)/) || []; //encontre este regex en internet, y quise entender pq tiraba tres resultados, so down the rabbit hole we go brb
+      //despues de un curso intensivo de regex, ya entendi match me devuelve primero el match de la regex literal, que en este caso seria cualquier string que contenga
+      //una substring opcional de cualquier caracter hasta encontrar un :, luego la segunda substring que puede contener inclusive otro :, en este caso, responderia primero
+      //con la combinacion entera de username:password, pero como esta configurada para buscar por grupos, (), match me devuelve como variables adicionales los grupos de datos
+
+      //debido a vulnerabilidadeas de timing attacks, es necesario corroborar la igualdad de passwords a traves de un algoritmo de tiempo constante
+      const secretByteLen = Buffer.byteLength(mockedCredentials.password);
+      const inputByteLen = Buffer.byteLength(decodedPassword);
+
+      const secretBuffer = Buffer.alloc(secretByteLen, 0, "utf8");
+      secretBuffer.write(mockedCredentials.password);
+
+      const inputBuffer = Buffer.alloc(inputByteLen, 0, "utf8");
+      inputBuffer.write(decodedPassword);
+
+      if (
+        crypto.timingSafeEqual(inputBuffer, secretBuffer) &&
+        secretByteLen === inputByteLen &&
+        mockedCredentials.username === decodedUsername
+      ) {
+        res.status(200).send("Authenticated");
+      } else {
+        res.status(401).send("Failed authentication");
+      }
     }
   } catch (err) {}
 }
